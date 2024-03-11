6a513e397e57db7fb39e7103a33eb806e89086f9
Vann06 <127684976+Vann06@users.noreply.github.com>
2024-03-11 17:28:07 -0600

    LispFunction

--- proyecto1/src/main/java/uv/edu/gt/LispFunction.java
+++ proyecto1/src/main/java/uv/edu/gt/LispFunction.java
@@ -1,5 +1,39 @@
 package uv.edu.gt;
 
 public class LispFunction {
-    
+
+    private String name;
+    private List<String> parameters;
+    private LispExpression body;
+
+
+
+    public LispExpression apply(List<LispExpression> arguments, Environment env){
+
+        return null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String Name){
+        this.name = name;
+    }
+
+    public List<String> getParameters(){
+        return parameters;
+    }
+
+    public void setParameters(List<String> parameters){
+        this.parameters = parameters;
+    }
+
+    public LispExpression getBody() {
+        return body;
+    }
+
+    public void setBody(LispExpression body){
+        this.body = body;
+    }
 }
